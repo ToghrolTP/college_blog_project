29779aa9b18cafbc695e166edc0fc258feffaef3
toghroltp <toghroltheprogrammer@gmail.com>
2025-12-18 02:32:45 +0330

    Add delete post functionality

---
+++ delete.php
@@ -0,0 +1,25 @@
+<?php
+include 'config.php';
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    
+    $sql = "DELETE FROM posts WHERE id = ?";
+    
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $id);
+    
+    if ($stmt->execute()) {
+        header("Location: index.php");
+        exit();
+    } else {
+        die("Error deleting post: " . $conn->error);
+    }
+    
+    $stmt->close();
+    $conn->close();
+    
+} else {
+    die("No post ID provided!");
+}
+?>
