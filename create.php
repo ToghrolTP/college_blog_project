033b05799108b743aad395deba729871a9f37248
toghroltp <toghroltheprogrammer@gmail.com>
2025-12-18 12:30:03 +0330

    Apply Gruvbox styling to all pages

--- create.php
+++ create.php
@@ -64,6 +64,8 @@ $conn->close();
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <?php endif; ?>
     
+    <link href="style.css" rel="stylesheet">
+    
     <?php if (get_direction() == 'rtl'): ?>
     <style>
         body {
--- index.php
+++ index.php
@@ -34,6 +34,7 @@ $result = $stmt->get_result();
     <?php else: ?>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <?php endif; ?>
+    
     <link href="style.css" rel="stylesheet">
     
     <?php if (get_direction() == 'rtl'): ?>
